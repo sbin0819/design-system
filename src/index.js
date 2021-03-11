150d60b20a4cbcf851a353589ced03a1e0c3a107
sbin0819 <sbinha123@gmail.com>
2021-03-11 21:09:11 +0900

    before theme

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import {
+  PrimaryButton,
+  SecondaryButton,
+  TertiaryButton,
+} from './components/Button';
+import { GlobalStyle, darkTheme, defaultTheme } from './utils';
+
+const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
+
+  return (
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button
+        style={{
+          margin: '0 16px 24px',
+          padding: '8px',
+          background: 'none',
+        }}
+        onClick={() => setUseDarkTheme(true)}
+      >
+        Dark theme
+      </button>
+      <button
+        style={{
+          margin: '0 16px 24px',
+          padding: '8px',
+          background: 'none',
+        }}
+        onClick={() => setUseDarkTheme(false)}
+      >
+        default theme
+      </button>
+      <div
+        style={{
+          background: useDarkTheme
+            ? defaultTheme.primaryColor
+            : darkTheme.primaryColor,
+          width: '100vw',
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        }}
+      >
+        <PrimaryButton modifiers={['small', 'success', 'primaryButtonSuccess']}>
+          Hello world
+        </PrimaryButton>
+        <SecondaryButton
+          modifiers={['large', 'warning', 'secondaryButtonWarning']}
+        >
+          Goodbye world
+        </SecondaryButton>
+        <TertiaryButton modifiers={['error', 'tertiaryButtonError']}>
+          Hey world
+        </TertiaryButton>
+        <GlobalStyle />
+      </div>
+    </ThemeProvider>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
