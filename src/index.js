0b041ca4a5148b2e3d469e931c4df26b209a9a20
sbin0819 <sbinha123@gmail.com>
2021-03-12 20:50:10 +0900

    done theme

--- src/index.js
+++ src/index.js
@@ -10,28 +10,19 @@ import { GlobalStyle, darkTheme, defaultTheme } from './utils';
 
 const App = () => {
   const [useDarkTheme, setUseDarkTheme] = useState(false);
-
   return (
     <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
       <button
-        style={{
-          margin: '0 16px 24px',
-          padding: '8px',
-          background: 'none',
-        }}
+        style={{ margin: '0 16px 24px', padding: '8px', background: 'none' }}
         onClick={() => setUseDarkTheme(true)}
       >
         Dark theme
       </button>
       <button
-        style={{
-          margin: '0 16px 24px',
-          padding: '8px',
-          background: 'none',
-        }}
+        style={{ margin: '0 16px 24px', padding: '8px', background: 'none' }}
         onClick={() => setUseDarkTheme(false)}
       >
-        default theme
+        Default theme
       </button>
       <div
         style={{
@@ -39,23 +30,15 @@ const App = () => {
             ? defaultTheme.primaryColor
             : darkTheme.primaryColor,
           width: '100vw',
-          height: '100vh',
+          height: '90vh',
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'space-around',
         }}
       >
-        <PrimaryButton modifiers={['small', 'success', 'primaryButtonSuccess']}>
-          Hello world
-        </PrimaryButton>
-        <SecondaryButton
-          modifiers={['large', 'warning', 'secondaryButtonWarning']}
-        >
-          Goodbye world
-        </SecondaryButton>
-        <TertiaryButton modifiers={['error', 'tertiaryButtonError']}>
-          Hey world
-        </TertiaryButton>
+        <PrimaryButton modifiers={['small']}>Hello World</PrimaryButton>
+        <SecondaryButton modifiers={['large']}>Goodbye World</SecondaryButton>
+        <TertiaryButton>Hey</TertiaryButton>
         <GlobalStyle />
       </div>
     </ThemeProvider>
